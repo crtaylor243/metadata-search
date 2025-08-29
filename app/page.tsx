147bc3698413bbe7e21f2d77adbf52613a8a3ea4
Ryan Taylor <rtaylor@gitlab.com>
2025-08-29 13:26:58 -0500

    disable artist search

--- app/page.tsx
+++ app/page.tsx
@@ -128,10 +128,12 @@ export default function Home() {
             <Tag className="w-4 h-4" />
             Label Search
           </TabsTrigger>
-          <TabsTrigger value="artist" className="flex items-center gap-2">
-            <Music className="w-4 h-4" />
-            Artist Search
-          </TabsTrigger>
+          {false && (
+            <TabsTrigger value="artist" className="flex items-center gap-2">
+              <Music className="w-4 h-4" />
+              Artist Search
+            </TabsTrigger>
+          )}
         </TabsList>
 
         <TabsContent value="label">
